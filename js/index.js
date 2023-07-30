92fba58b74a7542ffcf085a689e9f4df90b5fbb7
may <altamiranormay@gmail.com>
2023-07-30 16:54:33 -0700

    tag bar is done, next step: link to post

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
 import {getJson} from "./api.js";
 
-const printPostDetail = (postObject, post, key) =>
+const printFirstTagMiniPost = (postObject, firstTag, secondTag) =>
 {
   let {title, comments,tags} = postObject;
   for(let i=0; i<tags.length; i++) {
-  if (tags[i].includes('#beginners')) {
-     document.getElementById('title-post').innerText = title; 
-     document.getElementById('comments-post').innerText = comments;
-     console.log('key desde el for`',key, 'post desde el for', post)
+  if (tags[i].includes(firstTag)) {
+
+     let tagsContainer = document.getElementById('tags-container');
+     let tagContainer = document.createElement('a');
+     tagContainer.classList.add('list-group-item');
+
+     let titleContainer = document.createElement('div');
+     titleContainer.innerText = title;
+
+     let commentContainer = document.createElement('div');
+     commentContainer.innerText = comments + ' comments';
+
+     tagContainer.append(titleContainer, commentContainer);
+     tagsContainer.append(tagContainer);
+     
     }
+  if (tags[i].includes(secondTag)) {
+
+        let tagsContainer = document.getElementById('second-tags-container');
+        let tagContainer = document.createElement('a');
+        tagContainer.classList.add('list-group-item');
+   
+        let titleContainer = document.createElement('div');
+        titleContainer.innerText = title;
+   
+        let commentContainer = document.createElement('div');
+        commentContainer.innerText = comments + ' comments';
+   
+        tagContainer.append(titleContainer, commentContainer);
+        tagsContainer.append(tagContainer);
+        
+       }
   }
 }
 
@@ -18,14 +45,14 @@ const printPost = async () => {
     let keys = Object.keys(post);
     console.log('keys', keys);
   
-    let cardWrapper = document.getElementById('tag-container');
+    let cardWrapper = document.getElementById('tags-container'); 
   
   
     keys.forEach((key) => {
         console.log('key', key);
         console.log('post',post[key]);
 
-        let card = printPostDetail(post[key], post, key); //el post[key] es el argumento q queremos iterar
+        let card = printFirstTagMiniPost(post[key], '#css', '#devops'); //el post[key] es el argumento q queremos iterar
         console.log('card',card);
         cardWrapper.append(card);
     })      
