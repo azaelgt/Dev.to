e7e31dfc427b2e80858728ded4f1003295a0800f
HollowZll <118689735+HollowZll@users.noreply.github.com>
2023-07-31 20:08:38 -0600

    login token fake & login and sign up redirection

--- js/login.js
+++ js/login.js
@@ -2,14 +2,40 @@ const openInNewTab = (url) => {
     window.open(url, "_blank");
   }
 
-const btnLogin = document.getElementById('continue');
 const btnForgot = document.getElementById('forgot');
-
-btnLogin.addEventListener("click", () => {
-    openInNewTab("http://127.0.0.1:5501/index.html");
-  });
 
 btnForgot.addEventListener("click", () => {
     openInNewTab("http://127.0.0.1:5501/login.html");
   });
+
+  // fake token
+
+  function generateFakeToken () {
+    const characters = 'ABCDFEGHKLQMNOPRTdfsjhl;dasjfgldfsaniudh0123456789';
+    let token = '';
+    for (let i = 0; i < 32; i++) {
+      token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
+  }
+
+
+  //Login logic 
+
+
+  function handleLogic() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const fakeToken = generateFakeToken();
+
+    localStorage.setItem('authToken', fakeToken);
+
+    window.open("./index.html", "_blank");
+  }
+
+  const loginButton = document.getElementById('continue');
+  loginButton.addEventListener('click', handleLogic);
+
+
 
